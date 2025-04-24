e9e8797a9f0ed006dc63c8a5d27a05f619397e44
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2025-04-24 15:37:58 +0300

    feat(workflow-execution): add active workflow event

--- src/main/java/com/modula/common/domain/workflow/execution/events/ActivateWorkflowTask.java
+++ src/main/java/com/modula/common/domain/workflow/execution/events/ActivateWorkflowTask.java
@@ -11,6 +11,6 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ActivateWorkflow {
+public class ActivateWorkflowTask {
     private UUID workflowInstanceId;
 }
