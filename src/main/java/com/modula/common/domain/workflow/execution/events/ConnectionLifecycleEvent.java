a1f956bb2f874a181a63702caa104a27559d347c
wxqryy <vachitovaidar@gmail.com>
2025-09-01 13:49:35 +0500

    fix: added moduleName in ConnectionLifecycleEvent

--- src/main/java/com/modula/common/domain/workflow/execution/events/ConnectionLifecycleEvent.java
+++ src/main/java/com/modula/common/domain/workflow/execution/events/ConnectionLifecycleEvent.java
@@ -11,5 +11,6 @@ import lombok.NoArgsConstructor;
 public class ConnectionLifecycleEvent {
 
     private ConnectionEventType eventType;
+    private String moduleName;
     private ExternalConnectionDto connection;
 }
