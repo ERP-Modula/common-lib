c13cd8c0285c2fb92ea44a43daf51370a6900983
wxqryy <vachitovaidar@gmail.com>
2025-09-13 20:07:50 +0500

    updated ConnectionLifecycleEvent

--- src/main/java/com/modula/common/domain/workflow/execution/events/ConnectionLifecycleEvent.java
+++ src/main/java/com/modula/common/domain/workflow/execution/events/ConnectionLifecycleEvent.java
@@ -4,11 +4,13 @@ import com.modula.common.connections.dto.connection.ExternalConnectionDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ConnectionLifecycleEvent {
+    private UUID workspaceId;
     private ConnectionEventType eventType;
     private String moduleName;
     private ExternalConnectionDto connection;
