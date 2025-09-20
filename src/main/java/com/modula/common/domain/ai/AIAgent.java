096b676583870d74f6531c1cff9b9ec6955c140f
wxqryy <vachitovaidar@gmail.com>
2025-09-20 19:08:52 +0500

    feat(ai-agent): AiAgent entities

--- src/main/java/com/modula/common/domain/ai/AIAgent.java
+++ src/main/java/com/modula/common/domain/ai/AIAgent.java
@@ -3,6 +3,7 @@ package com.modula.common.domain.ai;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -25,7 +26,33 @@ public class AIAgent {
     private String systemPrompt;
 
     @Column(nullable = false)
-    private String llmModuleName;
+    private String llmProvider;
+
+    @Column(nullable = false)
+    private String llmModelName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conversation_memory_id")
+    private ConversationMemory conversationMemory;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "ai_agent_knowledge_bases",
+            joinColumns = @JoinColumn(name = "ai_agent_id"),
+            inverseJoinColumns = @JoinColumn(name = "knowledge_base_id")
+    )
+    private Set<KnowledgeBase> knowledgeBases;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "ai_agent_instruments",
+            joinColumns = @JoinColumn(name = "ai_agent_id"),
+            inverseJoinColumns = @JoinColumn(name = "instrument_id")
+    )
+    private Set<Instrument> instruments;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private boolean useInternetSearch;
 
     @Column(nullable = false)
     private UUID workspaceId;
