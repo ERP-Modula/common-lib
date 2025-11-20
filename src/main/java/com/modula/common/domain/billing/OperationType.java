78bb47f0e402d8b307c13f328464343fdcfef524
wxqryy <vachitovaidar@gmail.com>
2025-11-20 22:07:05 +0500

    fix: added JsonIgnoreProperties in OperationType

--- src/main/java/com/modula/common/domain/billing/OperationType.java
+++ src/main/java/com/modula/common/domain/billing/OperationType.java
@@ -1,5 +1,6 @@
 package com.modula.common.domain.billing;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class OperationType {
 
     @Id
