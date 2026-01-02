14bd59014179bbf7946f1dd2829a6d1f0aefcae2
wxqryy <vachitovaidar@gmail.com>
2026-01-02 17:30:37 +0500

    feat: added ExecuteOperationCommand

---
+++ src/main/java/com/modula/common/domain/billing/dto/command/ExecuteOperationCommand.java
@@ -0,0 +1,16 @@
+package com.modula.common.domain.billing.dto.command;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExecuteOperationCommand {
+    private UUID userId;
+    private String operationKey;
+    private String referenceId;
+    private String description;
+}
